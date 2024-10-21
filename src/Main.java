05083751aeffc3f974aeb83a5170fd674a353ea6
Engin Karatas <enginkaratas99@gmail.com>
2024-10-21 04:47:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("selam canim : d");
+		String test = "engino";
+		System.err.println(test);
+		
+		System.out.println("yokyava");
+		boolean laYeter = true;
+		System.out.println(laYeter);
+		
+		String[] krediler = {"walla", "niyeki", "öylemiymis"};
+		System.out.println(krediler[1]);
+	}
+
+}
