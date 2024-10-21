6390cd0c35b39e1bb79b380f7455f9afd2e6321a
Engin Karatas <enginkaratas99@gmail.com>
2024-10-21 23:59:43 +0300

    create getAll endpoint

--- jobland.backend/src/main/java/joblandsystem/jobland/backend/webApi/controllers/CompaniesController.java
+++ jobland.backend/src/main/java/joblandsystem/jobland/backend/webApi/controllers/CompaniesController.java
@@ -1,8 +1,23 @@
 package joblandsystem.jobland.backend.webApi.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //annotation(bilgilendirme ifadesi)
-public class CompaniesController {
+import joblandsystem.jobland.backend.business.abstracts.CompanyService;
+import joblandsystem.jobland.backend.entities.concrates.Company;
 
+@RestController //annotation(bilgilendirme ifadesi)//it means this is a controller point
+@RequestMapping("/api/companies")
+public class CompaniesController {
+	CompanyService companyService;
+	
+	public CompaniesController (CompanyService companyService)  {
+		companyService = companyService;
+	}
+	
+	public List<Company> getAll(){
+		return companyService.getAll();
+	}
 }
