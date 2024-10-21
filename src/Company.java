78c7c70acda026afe9e2e7c9260acfc634ab6cfb
Engin Karatas <enginkaratas99@gmail.com>
2024-10-21 05:21:27 +0300

    create Company.java

---
+++ src/Company.java
@@ -0,0 +1,7 @@
+
+public class Company {
+	String name;
+	String email;
+	String website;
+	String location;
+}
--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,17 @@ public class Main {
 		System.out.println("yokyava");
 		boolean laYeter = true;
 		System.out.println(laYeter);
-		
+		 
 		String[] krediler = {"walla", "niyeki", "öylemiymis"};
 		System.out.println(krediler[1]);
+		Company company = new Company();
+		company.name = "Canonical";
+		company.email = "career@canonical.com";
+		company.location = "UK";
+		company.website = "www.canonical.com";
+		
+		System.out.println(company.name + " application successful");
+				
 	}
 
 }
